b2d31b58cb3a0224fa13045d6a3d6467326cb56f
piyawatbol <93056207+piyawatbol@users.noreply.github.com>
2023-08-09 15:21:21 +0700

    success get_user edit profile edit image

--- routes/users/edit_user.js
+++ routes/users/edit_user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Users = require("../../models/Users");
 const multer = require("multer");
 const path = require("path");
+const auth = require("../../middleware/auth");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: "./images/users",
@@ -18,31 +20,53 @@ const upload = multer({
   storage: storage,
 });
 
-router.post("/:id", async (req, res) => {
-  const id = req.params.id;
+router.post("/", auth, async (req, res) => {
+  const { first_name, last_name, id_card, phone, email } = req.body;
+
+  if ((!first_name || !last_name || !id_card || !phone, !email)) {
+    return res.status(401).send("missing information");
+  }
+
+  const userId = req.user.user_id;
   try {
-    const data = await Users.findByIdAndUpdate(id ,{ $set: req.body }, { new: true });
+    const data = await Users.findByIdAndUpdate(
+      userId,
+      { $set: req.body },
+      { new: true }
+    );
     if (!data) {
-      return res.status(401).send(data);
+      return res.status(401).send("user not edit");
     }
-    res.send(data);
+    res.status(201).send("edit success");
   } catch (err) {
     console.log(err);
   }
 });
 
-router.post("/image/:id",upload.single("image"), async (req, res) => {
-    const id = req.params.id;
-    const image = req.file.filename;
-    try {
-      const data = await Users.findByIdAndUpdate(id ,{ $set: {user_img: image }}, { new: true });
-      if (!data) {
-        return res.status(401).send(data);
-      }
-      res.send(data);
-    } catch (err) {
-      console.log(err);
+router.post("/image", auth, upload.single("image"), async (req, res) => {
+  const userId = req.user.user_id;
+  const image = req.file.filename;
+  console.log(image);
+  try {
+    const userData  = await Users.findById(userId);
+    const oldImage =  userData.user_img ;
+    var filePath = `../../images/users/${oldImage}`;
+    var imagePage = path.join(__dirname,filePath);
+    if(oldImage){
+      fs.unlinkSync(imagePage)
     }
-  });
+    const data = await Users.findByIdAndUpdate(
+      userId,
+      { $set: { user_img: image } },
+      { new: true }
+    );
+    if (!data) {
+      return res.status(401).send('edit image something wrong');
+    }
+    res.status(201).send("edit image success")
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
